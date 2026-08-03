f0f2b6a6ad1d7ac02c0c889fc335ff66b8852752
ElGatoPro300 <catgamer207@gmail.com>
2026-08-03 14:40:52 -0600

    fix timeline ruler

--- src/client/java/mchorse/bbs_mod/ui/utils/TimelineRuler.java
+++ src/client/java/mchorse/bbs_mod/ui/utils/TimelineRuler.java
@@ -5,18 +5,11 @@ import mchorse.bbs_mod.BBSSettings;
 public class TimelineRuler
 {
     private static final int MIN_LABEL_SPACING = 70;
-    private static final int[] SECOND_STEPS = {1, 2, 5, 10, 20, 100, 200, 600, 1200, 6000};
+    private static final int[] SECOND_STEPS = {1, 2, 5, 10, 20, 40, 100, 200, 600, 1200, 6000};
 
     public static Step steps(Scale scale)
     {
-        if (BBSSettings.editorTimeMode != null && BBSSettings.editorTimeMode.get() == 1)
-        {
-            return secondsSteps(scale);
-        }
-
-        int minor = Math.max(1, scale.getMult());
-
-        return new Step(minor, minor * 5);
+        return secondsSteps(scale);
     }
 
     private static Step secondsSteps(Scale scale)
