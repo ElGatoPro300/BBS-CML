5dda612cd3e1b6e8762091c4acb7548fa06687bc
ElGatoPro300 <catgamer207@gmail.com>
2026-08-16 15:33:57 -0600

    fix

--- src/client/java/mchorse/bbs_mod/forms/renderers/FluidFormRenderer.java
+++ src/client/java/mchorse/bbs_mod/forms/renderers/FluidFormRenderer.java
@@ -122,6 +122,18 @@ public class FluidFormRenderer extends FormRenderer<FluidForm> implements ITicka
             Matrix4f matrix = stack.peek().getPositionMatrix();
             Matrix3f normal = stack.peek().getNormalMatrix();
             
+            float nx1 = normal.m20();
+            float ny1 = normal.m21();
+            float nz1 = normal.m22();
+
+            float nx2 = normal.m10();
+            float ny2 = normal.m11();
+            float nz2 = normal.m12();
+
+            float nx3 = normal.m00();
+            float ny3 = normal.m01();
+            float nz3 = normal.m02();
+            
             for (int i = 0; i < segments; i++)
             {
                 float a1 = (float) (i * Math.PI * 2 / segments);
@@ -133,16 +145,16 @@ public class FluidFormRenderer extends FormRenderer<FluidForm> implements ITicka
                 float s2 = (float) Math.sin(a2) * r;
                 
                 /* XY circle */
-                builder.vertex(matrix, c1, s1, 0).color(1f, 0f, 0f, 1f).normal(normal, 0, 0, 1).next();
-                builder.vertex(matrix, c2, s2, 0).color(1f, 0f, 0f, 1f).normal(normal, 0, 0, 1).next();
+                builder.vertex(matrix, c1, s1, 0).color(1f, 0f, 0f, 1f).normal(nx1, ny1, nz1).next();
+                builder.vertex(matrix, c2, s2, 0).color(1f, 0f, 0f, 1f).normal(nx1, ny1, nz1).next();
                 
                 /* XZ circle */
-                builder.vertex(matrix, c1, 0, s1).color(1f, 0f, 0f, 1f).normal(normal, 0, 1, 0).next();
-                builder.vertex(matrix, c2, 0, s2).color(1f, 0f, 0f, 1f).normal(normal, 0, 1, 0).next();
+                builder.vertex(matrix, c1, 0, s1).color(1f, 0f, 0f, 1f).normal(nx2, ny2, nz2).next();
+                builder.vertex(matrix, c2, 0, s2).color(1f, 0f, 0f, 1f).normal(nx2, ny2, nz2).next();
                 
                 /* YZ circle */
-                builder.vertex(matrix, 0, c1, s1).color(1f, 0f, 0f, 1f).normal(normal, 1, 0, 0).next();
-                builder.vertex(matrix, 0, c2, s2).color(1f, 0f, 0f, 1f).normal(normal, 1, 0, 0).next();
+                builder.vertex(matrix, 0, c1, s1).color(1f, 0f, 0f, 1f).normal(nx3, ny3, nz3).next();
+                builder.vertex(matrix, 0, c2, s2).color(1f, 0f, 0f, 1f).normal(nx3, ny3, nz3).next();
             }
             
             stack.pop();
@@ -694,12 +706,15 @@ public class FluidFormRenderer extends FormRenderer<FluidForm> implements ITicka
 
     private void addVertex(BufferBuilder builder, Matrix4f matrix, Matrix3f normal, float x, float y, float z, float u, float v, Color color, int overlay, int light, Vector3f n)
     {
+         Vector3f tn = new Vector3f(n);
+         tn.mul(normal);
+
          builder.vertex(matrix, x, y, z)
                .color((int) (color.r * 255), (int) (color.g * 255), (int) (color.b * 255), (int) (color.a * 255))
                .texture(u, v)
                .overlay(overlay)
                .light(light)
-               .normal(normal, n.x, n.y, n.z)
+               .normal(tn.x, tn.y, tn.z);
                .next();
     }
     
