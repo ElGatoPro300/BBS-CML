c8dba6ec861dec9b18aa3b04a206ae1f1bdda29f
ElGatoPro300 <catgamer207@gmail.com>
2026-08-17 11:37:38 -0600

    recover vanilla eyeshadows

--- src/client/java/mchorse/bbs_mod/mixin/client/EntityRendererDispatcherInvoker.java
+++ src/client/java/mchorse/bbs_mod/mixin/client/EntityRendererDispatcherInvoker.java
@@ -2,11 +2,9 @@ package mchorse.bbs_mod.mixin.client;
 
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import net.minecraft.world.WorldView;
-
-import org.joml.Quaternionf;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -14,11 +12,9 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(EntityRenderDispatcher.class)
 public interface EntityRendererDispatcherInvoker
 {
-    /* @Invoker("renderShadow")
-    public static void bbs$renderShadow(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, float opacity, float tickDelta, WorldView world, float radius)
-    {} */
-
-    /* TODO 1.21.4: renderFire removed from EntityRenderDispatcher */
-    // @Invoker("renderFire")
-    // void bbs$renderFire(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, Quaternionf rotation);
-}
+    @Invoker("renderShadow")
+    public static void bbs$renderShadow(MatrixStack matrices, VertexConsumerProvider vertexConsumers, EntityRenderState state, float opacity, float tickDelta, WorldView world, float radius)
+    {
+        throw new AssertionError();
+    }
+}
