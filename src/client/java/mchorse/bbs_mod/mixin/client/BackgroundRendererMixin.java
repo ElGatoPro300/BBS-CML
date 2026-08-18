4a1e15829a02de406c973521a41fa532e4baae79
ElGatoPro300 <catgamer207@gmail.com>
2026-08-17 18:22:58 -0600

    fix crash

--- src/client/java/mchorse/bbs_mod/mixin/client/BackgroundRendererMixin.java
+++ src/client/java/mchorse/bbs_mod/mixin/client/BackgroundRendererMixin.java
@@ -1,6 +1,11 @@
 package mchorse.bbs_mod.mixin.client;
 
+import net.minecraft.client.render.WorldRenderer;
+
+import org.spongepowered.asm.mixin.Mixin;
+
 /* BackgroundRenderer was removed in 1.21.11 Yarn */
+@Mixin(WorldRenderer.class)
 public class BackgroundRendererMixin
 {
 }
