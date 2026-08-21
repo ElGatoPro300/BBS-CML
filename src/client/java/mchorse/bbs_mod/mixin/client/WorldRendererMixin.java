e581c67a036e1f1dfc73868c2f8ad0634cb15c08
ElGatoPro300 <catgamer207@gmail.com>
2026-08-21 14:48:17 -0600

    fix crash

--- src/client/java/mchorse/bbs_mod/mixin/client/WorldRendererMixin.java
+++ src/client/java/mchorse/bbs_mod/mixin/client/WorldRendererMixin.java
@@ -8,16 +8,23 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.option.CloudRenderMode;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.DefaultFramebufferSet;
 import net.minecraft.client.render.FrameGraphBuilder;
+import net.minecraft.client.render.FramePass;
 import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Vec3d;
 
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import com.mojang.blaze3d.systems.RenderSystem;
+
 import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL11;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,20 +33,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin
 {
+    @Shadow private DefaultFramebufferSet framebufferSet;
 
-    @Inject(method = "renderSky(Lnet/minecraft/client/render/FrameGraphBuilder;Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/render/Fog;)V", at = @At("HEAD"), cancellable = true, require = 0)
-    public void onRenderSky(FrameGraphBuilder frameGraphBuilder, Camera camera, float tickDelta, Fog fog, CallbackInfo info)
+    @Inject(method = "renderSky", at = @At("HEAD"), cancellable = true, require = 0)
+    public void onRenderSky(FrameGraphBuilder frameGraphBuilder, Camera camera, GpuBufferSlice fogBuffer, CallbackInfo info)
     {
         if (BBSRendering.isChromaSkyEnabled())
         {
-            RenderPass pass = frameGraphBuilder.createPass("sky");
+            FramePass pass = frameGraphBuilder.createPass("sky");
 
             this.framebufferSet.mainFramebuffer = pass.transfer(this.framebufferSet.mainFramebuffer);
             pass.setRenderer(() -> {
                 Color color = Color.rgb(BBSRendering.getChromaSkyColor());
-
-                RenderSystem.clearColor(color.r, color.g, color.b, 1F);
-                RenderSystem.clear(GL11.GL_COLOR_BUFFER_BIT);
+                int colorInt = color.getRGBAColor();
+                Framebuffer mainFb = MinecraftClient.getInstance().getFramebuffer();
+                if (mainFb != null && mainFb.getColorAttachment() != null)
+                {
+                    RenderSystem.getDevice().createCommandEncoder().clearColorTexture(mainFb.getColorAttachment(), colorInt);
+                }
             });
 
             info.cancel();
@@ -47,7 +58,7 @@ public class WorldRendererMixin
     }
 
     @Inject(method = "renderClouds", at = @At("HEAD"), cancellable = true, require = 0)
-    public void onRenderClouds(FrameGraphBuilder frameGraphBuilder, Matrix4f modelViewMatrix, Matrix4f projectionMatrix, CloudRenderMode cloudRenderMode, Vec3d cameraPos, float tickDelta, int color, float cloudHeight, CallbackInfo info)
+    public void onRenderClouds(FrameGraphBuilder frameGraphBuilder, CloudRenderMode cloudRenderMode, Vec3d cameraPos, long ticks, float tickDelta, int color, float cloudHeight, CallbackInfo info)
     {
         if (BBSRendering.isChromaSkyEnabled() && !BBSRendering.isChromaSkyClouds())
         {
@@ -56,7 +67,7 @@ public class WorldRendererMixin
     }
 
     @Inject(method = "renderWeather", at = @At("HEAD"), cancellable = true, require = 0)
-    public void onRenderWeather(FrameGraphBuilder frameGraphBuilder, Vec3d cameraPos, float tickDelta, Fog fog, CallbackInfo info)
+    public void onRenderWeather(FrameGraphBuilder frameGraphBuilder, GpuBufferSlice fogBuffer, CallbackInfo info)
     {
         if (BBSRendering.shouldHideChromaTerrain())
         {
@@ -98,7 +109,7 @@ public class WorldRendererMixin
     }
 
     @Inject(at = @At("RETURN"), method = "onResized")
-    private void onResized(CallbackInfo info)
+    private void onResized(int width, int height, CallbackInfo info)
     {
         /*
         if (this.entityOutlinesFramebuffer == null)
