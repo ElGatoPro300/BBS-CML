8447800553a7513412dd4d194e955d5ad7a88a1f
ElGatoPro300 <catgamer207@gmail.com>
2026-09-08 18:50:43 -0600

    Fix model clipping in the interface

--- src/client/java/mchorse/bbs_mod/ui/home/UIHomePanel.java
+++ src/client/java/mchorse/bbs_mod/ui/home/UIHomePanel.java
@@ -27,9 +27,10 @@ import mchorse.bbs_mod.ui.framework.elements.overlay.UICreateAssetOverlayPanel;
 import mchorse.bbs_mod.ui.framework.elements.overlay.UIOverlay;
 import mchorse.bbs_mod.ui.framework.elements.overlay.UIPromptOverlayPanel;
 import mchorse.bbs_mod.ui.framework.elements.overlay.UISoundOverlayPanel;
+import mchorse.bbs_mod.forms.FormUtilsClient;
+import mchorse.bbs_mod.forms.forms.ModelForm;
 import mchorse.bbs_mod.ui.framework.elements.utils.UILabel;
 import mchorse.bbs_mod.ui.framework.elements.utils.UIRenderable;
-import mchorse.bbs_mod.ui.model.UIModelPreviewRenderer;
 import mchorse.bbs_mod.ui.utils.icons.Icon;
 import mchorse.bbs_mod.ui.utils.icons.Icons;
 import mchorse.bbs_mod.utils.DataPath;
@@ -1060,6 +1061,8 @@ public class UIHomePanel extends UIDashboardPanel
         {
             UIElement card = new UIElement()
             {
+                private ModelForm modelForm;
+
                 @Override
                 public boolean subMouseClicked(UIContext context)
                 {
@@ -1097,12 +1100,12 @@ public class UIHomePanel extends UIDashboardPanel
 
                     this.area.render(context.batcher, Colors.setA(0, 0.3F));
                     context.batcher.box(this.area.x, this.area.y, this.area.ex(), this.area.y + CARD_SIZE, Colors.setA(0, 0.2F));
-                    
+
                     if (selected || hover)
                     {
                         context.batcher.box(this.area.x, this.area.y, this.area.ex(), this.area.y + CARD_SIZE, Colors.A25);
                     }
-                    
+
                     context.batcher.box(this.area.x, this.area.y + CARD_SIZE, this.area.ex(), this.area.ey(), Colors.A50);
 
                     super.render(context);
@@ -1114,7 +1117,8 @@ public class UIHomePanel extends UIDashboardPanel
 
                         if (thumbnail != null)
                         {
-                            int w = CARD_SIZE - 4;
+                            int maxW = this.area.w - 4;
+                            int w = maxW;
                             int h = (int) (w * (thumbnail.height / (float) thumbnail.width));
 
                             if (h > CARD_SIZE - 4)
@@ -1123,7 +1127,7 @@ public class UIHomePanel extends UIDashboardPanel
                                 w = (int) (h * (thumbnail.width / (float) thumbnail.height));
                             }
 
-                            int ix = this.area.x + 2 + (CARD_SIZE - 4 - w) / 2;
+                            int ix = this.area.x + 2 + (maxW - w) / 2;
                             int iy = this.area.y + 2 + (CARD_SIZE - 4 - h) / 2;
 
                             context.batcher.fullTexturedBox(thumbnail, ix, iy, w, h);
@@ -1141,7 +1145,21 @@ public class UIHomePanel extends UIDashboardPanel
                     {
                         this.renderIcon(context, Icons.SOUND);
                     }
-                    /* Models render through the embedded UIModelPreviewRenderer child */
+                    else if (entry.type == ContentType.MODELS)
+                    {
+                        if (this.modelForm == null)
+                        {
+                            this.modelForm = new ModelForm();
+                            this.modelForm.model.set(entry.id);
+                        }
+
+                        int tx = this.area.x + 2;
+                        int ty = this.area.y + 2;
+                        int tw = CARD_SIZE - 4;
+                        int th = CARD_SIZE - 4;
+
+                        FormUtilsClient.renderUICachedStatic(this.modelForm, context, tx, ty, tx + tw, ty + th);
+                    }
 
                     String label = new DataPath(entry.id).getLast();
                     int maxW = this.area.w - 4;
@@ -1179,15 +1197,6 @@ public class UIHomePanel extends UIDashboardPanel
             card.context((menu) -> menu.action(Icons.REMOVE, UIKeys.FILM_HOME_REMOVE_RECENT,
                 () -> UIRecentMosaicGrid.this.home.removeFromRecent(entry)));
             card.relative(this).x(cx).y(cy).w(CARD_SIZE).h(CARD_SIZE + CARD_LABEL_H);
-
-            if (entry.type == ContentType.MODELS)
-            {
-                UIModelPreviewRenderer preview = new UIModelPreviewRenderer();
-
-                preview.relative(card).x(2).y(2).w(CARD_SIZE - 4).h(CARD_SIZE - 4);
-                preview.setModel(entry.id);
-                card.add(preview);
-            }
 
             return card;
         }
