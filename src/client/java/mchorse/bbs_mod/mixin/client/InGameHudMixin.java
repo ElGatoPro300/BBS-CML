62ef72a7fa0d329729897f57a4a13c49ffb0ced3
ElGatoPro300 <catgamer207@gmail.com>
2026-09-09 17:55:15 -0600

    fix film 26.1

--- src/client/java/mchorse/bbs_mod/mixin/client/InGameHudMixin.java
+++ src/client/java/mchorse/bbs_mod/mixin/client/InGameHudMixin.java
@@ -4,6 +4,8 @@ import mchorse.bbs_mod.BBSModClient;
 import mchorse.bbs_mod.camera.controller.ICameraController;
 import mchorse.bbs_mod.camera.controller.PlayCameraController;
 import mchorse.bbs_mod.client.BBSRendering;
+import mchorse.bbs_mod.ui.framework.UIBaseMenu;
+import mchorse.bbs_mod.ui.framework.UIScreen;
 
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
@@ -21,8 +23,9 @@ public class InGameHudMixin
     public void render(GuiGraphicsExtractor drawContext, DeltaTracker tickCounter, CallbackInfo info)
     {
         ICameraController current = BBSModClient.getCameraController().getCurrent();
+        UIBaseMenu menu = UIScreen.getCurrentMenu();
 
-        if (current instanceof PlayCameraController)
+        if (current instanceof PlayCameraController || (menu != null && menu.canHideHUD()))
         {
             BBSRendering.onRenderBeforeScreen();
 
